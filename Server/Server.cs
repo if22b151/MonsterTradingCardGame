0252cfd45f104d7ce35c2a59b55e92b9c52a7b53
PaulFel123 <if22b151@technikum-wien.at>
2023-10-28 14:33:57 +0200

    Created Server Project with some classes implemented

--- Server/Server.cs
+++ Server/Server.cs
@@ -23,59 +23,16 @@ namespace MCTGServer {
         }
 
         private void StartServerLoop() {
-            while(true) {
-                var clientSocket = _listener.AcceptTcpClient();
-                using var writer = new StreamWriter(clientSocket.GetStream()) { AutoFlush = true };
-                using var reader = new StreamReader(clientSocket.GetStream());
-
-                string? line;
-
-                line = reader.ReadLine();
-                if(line != null) {
-                    Console.WriteLine(line);
-                }
-                int content_length = 0;
-
-                while((line = reader.ReadLine()) != null) {
-                    Console.WriteLine(line);
-                    if(line == "") {
-                        break;
-                    }
-                    var parts = line.Split(':');
-                    if(parts.Length == 2 && parts[0] == "Content-Length") { 
-                        content_length = int.Parse(parts[1].Trim());
-                    }
-                }
-                if (content_length > 0) {
-                    var data = new StringBuilder(200);
-                    char[] chars = new char[1024];
-                    int bytesTotal = 0;
-                    while(bytesTotal < content_length) {
-                        var bytesRead = reader.Read(chars, 0, chars.Length);
-                        bytesTotal += bytesRead;
-                        if(bytesRead == 0) {
-                            break;
-                        }
-                        data.Append(chars, 0, bytesRead);
-                    }
-                    Console.WriteLine(data.ToString());
-                }
-
-                Console.WriteLine("----------------------------------------");
-
-                // ----- 3. Write the HTTP-Response -----
-                var writerAlsoToConsole = new StreamTracer(writer);  // we use a simple helper-class StreamTracer to write the HTTP-Response to the client and to the console
-
-                writerAlsoToConsole.WriteLine("HTTP/1.0 200 OK");    // first line in HTTP-Response contains the HTTP-Version and the status code
-                writerAlsoToConsole.WriteLine("Content-Type: text/html; charset=utf-8");     // the HTTP-headers (in HTTP after the first line, until the empy line)
-                writerAlsoToConsole.WriteLine();
-                writerAlsoToConsole.WriteLine("<html><body><h1>Hello World!</h1></body></html>");    // the HTTP-content (here we just return a minimalistic HTML Hello-World)
-
-                Console.WriteLine("========================================");
-
+            while (true) {
+                 var clientSocket = _listener.AcceptTcpClient();
+                ClientProcessor clientprocessor = new ClientProcessor();
+                clientprocessor.ClientProcessing(clientSocket);
             }
 
-
         }
+
+       
+
+        
     }
 }
